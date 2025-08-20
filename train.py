4f6621e67f57a682a80a50cda31bf665efeb083d
Arjun Kollipara <arjun.kollipara22@gmail.com>
2025-08-20 11:46:00 +0530

    extracting labels & features

--- train.py
+++ train.py
@@ -2,15 +2,14 @@ import h5py
 import pandas as pd
 import os
 
-csd_path = r"F:\EVERYTHING\arjun folder\college files\4 fourth year\SEM 7\major_project(batch25)\datasets\CMU-MOSEI\CMU_MOSEI_VisualFacet42.csd"
+csd_path = r"F:\EVERYTHING\arjun folder\college files\4 fourth year\SEM 7\major_project(batch25)\datasets\CMU-MOSEI\CMU_MOSEI_Labels.csd"
 output_dir = "data"
 os.makedirs(output_dir, exist_ok=True)
 
 with h5py.File(csd_path, "r") as f:
     key=f.keys()
     print(f"keys : {list(key)}")
-    facet_group = f['FACET 4.2']
-    print(f"glove_vectors_group keys : {list(facet_group.keys())}")
-    data_group = facet_group['data']
+    label_group = f['All Labels']
+    print(f"glove_vectors_group keys : {list(label_group.keys())}")
+    data_group = label_group['data']
     print(f"data_group keys : {list(data_group.keys())}")
-    
